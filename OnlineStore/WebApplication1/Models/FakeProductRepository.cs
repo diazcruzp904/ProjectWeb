895fb2285563e75fe3c35fec081832d193348149
diazcruzp904 <81534609+diazcruzp904@users.noreply.github.com>
2021-04-04 19:50:26 -0400

    First Version
    Stopped at Bullet 17

---
+++ OnlineStore/WebApplication1/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Product => new List<Product>
+        {
+            new Product { Name = "POP Tart Cherry", Price = 25},
+            new Product { Name = "POP Tart Chocolate", Price = 179},
+            new Product { Name = "POP Tart BlueBerry", Price = 95},
+        };
+    }
+}
